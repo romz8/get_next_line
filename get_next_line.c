25ee4247855b6f4194c56cb28aba23e48a85d222
Romain Jobert <rjobert@cbr5s6.42barcelona.com>
2023-05-31 22:11:50 +0200

    tentative try on create_chain : still not fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,34 @@
 /*   By: rjobert <rjobert@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:52:49 by rjobert           #+#    #+#             */
-/*   Updated: 2023/05/31 21:42:13 by rjobert          ###   ########.fr       */
+/*   Updated: 2023/05/31 22:10:43 by rjobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+void	CHECK_PRINT(t_list *byte)
+{
+	int i = 0;
+	while(byte)
+	{
+		printf("%i : %s\n", i, byte->buff);
+		i++;
+		byte = byte->next;
+	}
+}
+
 char	*get_next_line(int fd)
 {
 	int		size;
 	t_list	*byte_list;
 
-	byte_list = NULL;
+	if (fd < 0 || BUFFER < 0)
+		return (NULL);
 	size = 0;
-	byte_list = create_chain(byte_list, fd, &size);
-	printf("%i\n", size);
+	byte_list = create_chain(fd, &size);
+	CHECK_PRINT(byte_list);
+	//printf("%i\n", size);
 	return (all_line(byte_list, size));	
 }
 
@@ -37,25 +50,25 @@ t_list	*read_to_node(int fd, int *byte_read)
 	return (node);
 }
 	
-t_list	*create_chain(t_list *byte_list, int fd, int *size)
+t_list	*create_chain(int fd, int *size)
 {	
 	t_list 	*node;
+	t_list	*byte_list;
 	t_list	*head;
 	int		byte_read;
 
 	byte_read = 0;
-	head = byte_list;
-	byte_list = read_to_node(fd, &byte_read);
+	head = byte_read;read_to_node(fd, &byte_read);
 	while (byte_read != 0) 
 	{
-		printf("%i %s\n", byte_read, byte_list->buff);
 		node = read_to_node(fd, &byte_read);
 		byte_list->next = node;
 		byte_list = byte_list->next;
-		printf("%i %s\n", byte_read, byte_list->buff);
+		printf("%i : %s\n", byte_read, byte_list->buff);
 		if (ft_is_next_char(byte_list) == 0)
 			break ;
 	}
+	CHECK_PRINT(head);
 	return (head);
 }
 	
