513b884bf4f37956c509eb47c4533e20237fa22c
Romain Jobert <rjobert@student.42barcelona.com>
2023-06-14 18:50:19 +0200

    modified git structure

--- main_for_test/main.c
+++ main_for_test/main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include "../mandatory/get_next_line.h"
+#include "../get_next_line.h"
 #include <string.h>
 
 # define YELLOW "\033[1;33m"
 # define RED "\033[1;31m"
 # define RESET "\033[0m"
 
+//possible test : one_line_dune.txt / starwars.txt / seneca.txt / long_HP.txt
 
 int main(void)
 {
-	int fd = open("../test/long_HP.txt", O_RDONLY);
+	int fd = open("./test/starwars.txt", O_RDONLY);
 	char *line;
 	int i = 0;
-	while (i < 20)
+	while (i < 30)
 	{		
 		line = get_next_line(fd);
 		printf("LINE IS : %s", line);
--- main_for_test/main_bonus.c
+++ main_for_test/main_bonus.c
@@ -1,57 +1,53 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include "get_next_line.h"
+#include "../get_next_line_bonus.h"
 #include <string.h>
 
 # define YELLOW "\033[1;33m"
 # define RED "\033[1;31m"
 # define RESET "\033[0m"
-
+# define BBLU "\e[1;34m"
+# define BGRN "\e[1;32m"
+# define BCYN "\e[1;36m"
 
 int main(void)
 {
 	int fd = open("./test/long_HP.txt", O_RDONLY);
 	int fd2 = open("./test/starwars.txt", O_RDONLY);
+	int fd3 = open("./test/one_line_dune.txt", O_RDONLY);
+    int fd4 = open("./test/seneca.txt", O_RDONLY);
 	char *line;
+	
+	printf(YELLOW"\n\n TESTING ONE LINE FROM EACH FILE  ONE AT A TIME \n\n"RESET);
 	int i = 0;
-	/*
-	printf(YELLOW"\n\n TESTING TEXT IN SEQUENCE \n\n"RESET);
-	printf(RED"\n\n Harry Potter \n\n"RESET);
-	int i = 0;
-	while (i < 30)
-	{		
-		line = get_next_line(fd);
-		printf("LINE IS : %s", line);
-		free(line);
-		i++;
-	}
-	printf(RED"\n\n STAR WARS \n\n"RESET);
-	i = 0;
-	while (i < 30)
-	{		
-		line = get_next_line(fd2);
-		printf("LINE IS : %s", line);
-		free(line);
-		i++;
-	}*/
-	close (fd);
-	close (fd2);
-	printf(YELLOW"\n\n NOW TESTING ONE LEACH FOR EACH AND READIG AT THE SAME TIME \n\n"RESET);
-	fd = open("./test/long_HP.txt", O_RDONLY);
-	fd2 = open("./test/starwars.txt", O_RDONLY);
-	i = 0;
+	printf("fd HP is : %i and fd SW is : %i \n\n", fd, fd2);
 	while (i < 20)
-	{		
+	{
 		line = get_next_line(fd);
-		printf(RED"\n\n Harry Potter \n\n"RESET);
-		printf("LINE IS : %s", line);
+		printf(BBLU"LINE Harry Potter #%i : %s" RESET , i, line);
 		free(line);
-		printf(RED"\n\n STAR WARS \n\n"RESET);
+		printf("\n\n ======================= other fd line =======================\n\n");
+
 		line = get_next_line(fd2);
+		printf(RED"LINE IS STAR WARS #%i: %s"RESET, i, line);
 		free(line);
+		printf("\n\n ======================= other fd line =======================\n\n");
+
+		line = get_next_line(fd3);
+		printf(BGRN"LINE IS DUNE #%i: %s"RESET, i, line);
+		free(line);
+		printf("\n\n ======================= other fd line =======================\n\n");
+
+		line = get_next_line(fd4);
+		printf(BCYN"LINE IS SENECA #%i: %s"RESET, i, line);
+		free(line);
+		printf("\n\n ======================= other fd line =======================\n\n");
+
 		i++;
 	}
 	close(fd);
 	close(fd2);
+	close (fd3);
+	close(fd4);
 	return (0);
 }
